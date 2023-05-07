986b2899be14ecf9ec83c1a07aeb2500becf452e
velvettouch1004 <velvettouch1004@gmail.com>
2023-05-07 16:19:09 +0900

    First Commit

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ export DATABASE_PASSWORD=''
 
 export DATABASE_URL="mysql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:3306/graphql-example"
 
+npm run generate:typings
+
 npx prisma migrate dev --name init
 npx prisma generate
 
