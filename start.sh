04091f33597b98aa28c7c8cb7b81e08719cfdcea
velvettouch1004 <velvettouch1004@gmail.com>
2023-05-07 17:57:58 +0900

    Docker Compose

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
-export DATABASE_USER=''
-export DATABASE_PASSWORD=''
+export DATABASE_USER='root'
+export DATABASE_PASSWORD='root123'
+export DATABASE_PORT=3307
 
-export DATABASE_URL="mysql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:3306/graphql-example"
+export DATABASE_URL="mysql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:${DATABASE_PORT}/graphql-example"
+
+docker-compose up -d
 
 npm run generate:typings
 
